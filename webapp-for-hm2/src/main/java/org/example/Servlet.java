b00caefbd5e6fe74e33cfcf8c29ba6d56caf50c6
Ekaterina Poshelyuk <poshelyuk.katya@mail.ru>
2020-12-24 21:57:47 +0300

    first commit from master branch

---
+++ webapp-for-hm2/src/main/java/org/example/Servlet.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "echoServlet", urlPatterns = "/echo")
+public class Servlet extends HttpServlet{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello from my project</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
