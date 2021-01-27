df61bc71ad27577291d5bf16b48312769dd0ea3c
Ekaterina Poshelyuk <poshelyuk.katya@mail.ru>
2021-01-27 13:38:13 +0300

    Added dbunit tests

---
+++ home-work6-Hibernate/hibernate_first/src/test/java/it/academy/loader/PersonTestLoaderDbunit.java
@@ -0,0 +1,83 @@
+package it.academy.loader;
+
+import it.academy.pojos.Person;
+import it.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+
+import javax.net.ssl.HandshakeCompletedEvent;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class PersonTestLoaderDbunit extends BaseTest {
+
+
+    @Test
+    public void query() {
+
+        cleanInsert("PersonTestLoaderDbunit.xml");
+
+        List<Person> persons = session
+                .createQuery("from Person where surname like 'Tes%'", Person.class)
+                .list();
+
+        assertEquals(5, persons.size());
+        deleteDataset();
+    }
+
+    @Test
+    public void changeAge() {
+
+        cleanInsert("PersonTestLoaderDbunit.xml");
+        int id = 1;
+        int age = 99;
+
+        Transaction tx = session.beginTransaction();
+        Person person = session.get(Person.class, id);
+        person.setAge(age);
+        session.flush();
+        session.clear();
+        tx.commit();
+
+        assertEquals(99, (long) person.getAge());
+        deleteDataset();
+    }
+
+    @Test
+    public void changeName() {
+
+        cleanInsert("PersonTestLoaderDbunit.xml");
+        int id = 1;
+        String name = "Max";
+
+        Transaction tx = session.beginTransaction();
+        Person person = session.get(Person.class, id);
+        person.setName(name);
+        session.flush();
+        session.clear();
+        tx.commit();
+
+        assertEquals(name, person.getName());
+        deleteDataset();
+    }
+
+    @Test
+    public void changeSurname() {
+
+        cleanInsert("PersonTestLoaderDbunit.xml");
+        int id = 1;
+        String surname = "Maximov";
+
+        Transaction tx = session.beginTransaction();
+        Person person = session.get(Person.class, id);
+        person.setSurname(surname);
+        session.flush();
+        session.clear();
+        tx.commit();
+
+        assertEquals(person.getSurname(), surname);
+        deleteDataset();
+    }
+}
