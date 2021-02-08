6c793aeac8bb94446ae9191fef55b89bebeff415
Ekaterina Poshelyuk <poshelyuk.katya@mail.ru>
2021-02-08 14:03:58 +0300

    Modul 4. Tasks 8-10

---
+++ home-work8-Spring Annotations/spring-first/src/main/java/it/academy/Person.java
@@ -0,0 +1,34 @@
+package it.academy;
+
+import it.academy.pojos.IAddress;
+import it.academy.pojos.IEmail;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Person {
+
+    private Integer id;
+
+    private Integer age;
+
+    private String name;
+
+    private String surname;
+
+    @Autowired(required = false)
+    @Qualifier("firstAddress")
+    private IAddress address;
+
+    @Autowired
+    private IEmail email;
+
+}
---
+++ home-work8-Spring Annotations/spring-first/src/main/java/it/academy/SecondAddress.java
@@ -0,0 +1,15 @@
+package it.academy;
+
+import it.academy.pojos.IAddress;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecondAddress implements IAddress {
+
+    private Long id;
+    private String country;
+    private String city;
+    private String street;
+    private Long home;
+
+}
