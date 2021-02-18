8ff8b29cbc02264b58aaa2c83f1b164718d7adb8
Ekaterina Poshelyuk <poshelyuk.katya@mail.ru>
2021-02-18 14:26:12 +0300

    Modul4. Task16

---
+++ home-work9-Spring-MVC/tiles-app/src/main/java/it/academy/controller/TilesController.java
@@ -0,0 +1,17 @@
+package it.academy.controller;
+
+import org.springframework.data.repository.cdi.Eager;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TilesController {
+    @GetMapping("/")
+    public String homePage(ModelMap model) {
+
+        return "home";
+    }
+}
